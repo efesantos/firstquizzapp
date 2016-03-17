4fe913e82ee60ad76f0759d8089d2b7bbc33429e
Elmer <efe_santos@hotmail.com>
2016-03-17 12:11:45 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstquizzapp::Application.config.secret_key_base = '5610493a33d9bb56d337068d78a6dd828858feea7ec437ff746e27df1cfc85ddc99bf6f07de24e4bbf3e7083c3f92e58283dce6a4c1d73e18e1849ee68eb151d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firstquizzapp::Application.config.session_store :cookie_store, key: '_firstquizzapp_session'
